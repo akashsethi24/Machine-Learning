68f5d0683d4889bfec320081d47ee468bf2ec153
Akash Sethi <akashsethi@hotmail.com>
2016-06-28 18:28:10 +0530

    Method added for user rating input

--- src/main/scala/MovieRating.scala
+++ src/main/scala/MovieRating.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.mllib.recommendation.Rating
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -6,17 +7,34 @@ import org.apache.spark.{SparkConf, SparkContext}
   */
 class MovieRating {
 
+  val scanner = new java.util.Scanner(System.in)
   val conf = new SparkConf().setAppName("Recommendation App").setMaster("local")
   val sc = new SparkContext(conf)
   val movieLensHomeDir = "/Data/ml-20m"
 
-  def getTopTenMovies: Map[Int, Double] = {
+  def getRatings(moviesList: List[(Int, String, Double)]): List[Rating] = {
+    val data = moviesList.map { movie => {
+      println(s"So how much do you like to rate ${movie._2} Movie From 0 to 5 ?")
+      val rating = scanner.next().toDouble
+      new Rating(1001, movie._1, rating)
+    }
+    }
+    data
+  }
+
+  def getTopTenMovies: List[(Int, String, Double)] = {
 
     val ratings = sc.textFile(movieLensHomeDir + "/ratings.csv").map { line =>
       val fields = line.split(",")
       // format: (timestamp % 10, Rating(userId, movieId, rating))
       (fields(3).toLong % 10, Rating(fields(0).toInt, fields(1).toInt, fields(2).toDouble))
     }
+
+    val movies = sc.textFile(movieLensHomeDir + "/movies.csv").map { line =>
+      val fields = line.split(",")
+      (fields(0).toInt, fields(1))
+    }.collect.toMap
+
     val highestRatedCombined = ratings.map {
       case (timestamp, Rating(userId, movieId, rating)) => (movieId, rating)
     }.combineByKey(v => (v, 1),
@@ -34,20 +52,25 @@ class MovieRating {
     }.sortBy({ case ((movieId, agvRating)) => agvRating }, ascending = false).take(10)
     topTenMovies.foreach(println)
     highestRatedCombined.take(10).toMap
+
+    val moviesWithName = topTenMovies.toList.map { movie => (movie._1, movies.getOrElse(movie._1, "Not Found"), movie._2)
+    }
+    moviesWithName
   }
 
 }
 
+
 object MovieRating extends App {
 
-  val scanner = new java.util.Scanner(System.in)
+
   val ratingObj = new MovieRating
-  ratingObj.getTopTenMovies.foreach(println)
+  ratingObj.getRatings(ratingObj.getTopTenMovies).foreach(x => println(x.user + " - " + x.rating + " - " + x.product))
 
   println("for Spark Job info try http://localhost:4040")
   println("enter exit to end")
 
-  while (scanner.next() != "exit") {
+  while (ratingObj.scanner.next() != "exit") {
   }
   ratingObj.sc.stop
 }
